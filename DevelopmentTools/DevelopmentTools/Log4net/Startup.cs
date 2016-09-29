55947504be13aacc6dae57c8bf54cf83718dca18
Nayaata <n.m.keranova@gmail.com>
2016-09-29 16:53:30 +0300

    Upload DevelopmentTools homework

---
+++ DevelopmentTools/DevelopmentTools/Log4net/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace Log4Net
+{
+    using log4net;
+    using log4net.Config;
+    using System;
+
+    internal class Log4NetExample
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4NetExample));
+
+        internal static void Main()
+        {
+            XmlConfigurator.Configure();
+
+            Log.Info("Logging information!");
+
+            try
+            {
+                throw new Exception("Exception!");
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error message!", ex);
+            }
+
+            Console.WriteLine("Press any key to exit:");
+            Console.ReadKey();
+            Console.Clear();
+
+            /* or
+            BasicConfigurator.Configure();
+            Log.Debug("Debug msg");
+            Log.Error("Error msg");
+             */
+        }
+    }
+}
---
+++ DevelopmentTools/DevelopmentTools/T4Template/T4Template.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CarStore
+{
+	using System;
+
+	public class T4Example
+	{
+		    public int Car1 { get; set; }
+		    public int Car2 { get; set; }
+		    public int Car3 { get; set; }
+		    public int Car4 { get; set; }
+		    public int Car5 { get; set; }
+		}
+}
